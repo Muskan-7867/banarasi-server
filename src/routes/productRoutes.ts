54d61f24d6d656cc68a4a9ce259f88c368cb5343
Muskan-7867 <muskanloach984@gmail.com>
2025-08-23 16:21:07 +0530

    add route

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -4,13 +4,14 @@ import upload from "../middleware/multer";
 
 const router = Router();
 
-// Public routes
+
 router.post("/", upload.array("images", 10), productController.create);
 router.get("/", productController.getAll);
 router.get("/:id", productController.getById);
 router.put("/:id", upload.array("images", 10), productController.update);
 router.delete("/:id", productController.delete);
 router.get("/tag/:tag", productController.getProductByTag)
+router.post("/cartproducts", productController.getCartProducts)
 
 
 export default router;
